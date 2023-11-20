9475af5c66e1f5d0f80caf1e0df818b61c99e494
Matheus Henrique <mathttps@gmail.com>
2023-11-20 16:07:27 -0300

    teste

--- src/controllers/poll.js
+++ src/controllers/poll.js
@@ -5,10 +5,11 @@ import { ObjectId } from "mongodb";
 export async function createPoll(req, res) {
     const survey = req.body;
 
-    if(!survey.title) {
+    if (!survey.title) {
         res.status(422).send("Title não pode ser uma string vazia");
         return;
     }
+
     try {
         const expireAt = survey.expireAt || dayjs().add(1, "month").format("YYYY/MM/DD HH:mm");
         await dataBase.collection("polls").insertOne({ ...survey, expireAt });
@@ -23,26 +24,18 @@ export async function collectPoll(req, res) {
     res.send(surveys);
 }
 
-// //  const mostVotedChoice = votes.reduce((max, vote) => max.votes > vote.votes ? max : vote, { title: "", votes: -1 });
-// res.send({
-//     ...survey,
-//     result: mostVotedChoice,
-// });
-// } catch (err) {
-// res.status(500).send(err.message);
-
 export async function choiceIdPoll(req, res) {
-    
-    // const choice = res.locals.choice
-    // const poll = await pollCollection.findOne({ _id: ObjectId(res.locals.choice.pollId) })
-    // const isNewChoice = await choiceCollection.findOne({ title: choice.title })
-    // const now = dayjs()
+    const choice = res.locals.choice;
+    const pollCollection = dataBase.collection("polls");
+    const choiceCollection = dataBase.collection("choices");
 
     const pollId = req.params.id;
     try {
-        const survey = await dataBase.collection("polls").findOne({ _id: ObjectId(pollId) });
+        const survey = await pollCollection.findOne({ _id: ObjectId(pollId) });
+
         if (!survey) return res.status(404).send("Enquete não existe");
-        const choices = await dataBase.collection("choices").find({ pollId: ObjectId(pollId) }).toArray();
+
+        const choices = await choiceCollection.find({ pollId: ObjectId(pollId) }).toArray();
         res.send(choices);
     } catch (err) {
         res.status(500).send(err.message);
@@ -51,15 +44,24 @@ export async function choiceIdPoll(req, res) {
 
 export async function resultIdPoll(req, res) {
     const pollId = req.params.id;
+
     try {
         const survey = await dataBase.collection("polls").findOne({ _id: ObjectId(pollId) });
+
         if (!survey) return res.status(404).send("");
+
         const choices = await dataBase.collection("choices").find({ pollId: ObjectId(pollId) }).toArray();
+
         const votes = await Promise.all(choices.map(async (choice) => {
             const votesCount = await dataBase.collection("createChoices").countDocuments({ choiceId: choice._id });
             return { title: choice.title, votes: votesCount };
         }));
-        const mostVotedChoice = votes.reduce((max, vote) => max.votes > vote.votes ? max : vote, { title: "", votes: -1 });
+
+        const mostVotedChoice = votes.reduce((max, vote) => (max.votes > vote.votes ? max : vote), {
+            title: "",
+            votes: -1,
+        });
+
         res.send({
             ...survey,
             result: mostVotedChoice,
